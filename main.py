400200ea815608bd4022f41e9580ee944accbe2f
NashaFrith <44258016+NashaFrith@users.noreply.github.com>
2025-02-20 11:38:51 -0500

    Add shake and die functionality for random game choice

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from microbit import *
+#TO DO LIST
+#ADD SHAKE AND DIE FUNCTIONALITY 
+#MAKE STARTUP SHORTER
+#Add game choices
+die = 0
+
+def on_gesture_shake():
+    global die
+    die = randint(1,6)
+    basic.show_number(die)
+
 
 def minigames():
-    pass
+    global die
+    basic.show_string("WIN 5")
+    basic.show_string("SHAKE DIE")
+    input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+    while die == 0:
+        basic.pause(10)
+
+    basic.pause(1000)
+    #rock paper scissors, snake, don't look game,hop over game
 
 
 def main():
--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
+// TO DO LIST
+// ADD SHAKE AND DIE FUNCTIONALITY 
+// MAKE STARTUP SHORTER
+// Add game choices
+let die = 0
 function minigames() {
     
+    basic.showString("WIN 5")
+    basic.showString("SHAKE DIE")
+    input.onGesture(Gesture.Shake, function on_gesture_shake() {
+        
+        die = randint(1, 6)
+        basic.showNumber(die)
+    })
+    while (die == 0) {
+        basic.pause(10)
+    }
+    basic.pause(1000)
 }
 
+// rock paper scissors, snake, don't look game,hop over game
 function main() {
     for (let i = 0; i < 4; i++) {
         basic.showIcon(IconNames.Ghost)
