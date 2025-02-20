ea1034d22047aa58c87bed0ac53985c81457e6fa
NashaFrith <44258016+NashaFrith@users.noreply.github.com>
2025-02-20 00:02:48 -0500

    Create main function

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from microbit import *
 
+def minigames():
+    pass
+
+
+def main():
+    for i in range(4):
+        basic.show_icon(IconNames.GHOST)
+        basic.clear_screen()
+        basic.pause(200)
+        
+    basic.show_string('A 4 GAMES, B TO QUIT')
+    while True:
+        if input.button_is_pressed(Button.A):
+            minigames()
+        elif input.button_is_pressed(Button.B):
+            basic.show_string('BYE')
+
+main()
--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
+function minigames() {
+    
+}
 
+function main() {
+    for (let i = 0; i < 4; i++) {
+        basic.showIcon(IconNames.Ghost)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+    basic.showString("A 4 GAMES, B TO QUIT")
+    while (true) {
+        if (input.buttonIsPressed(Button.A)) {
+            minigames()
+        } else if (input.buttonIsPressed(Button.B)) {
+            basic.showString("BYE")
+        }
+        
+    }
+}
+
+main()
